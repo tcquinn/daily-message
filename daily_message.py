f442f486b63c0a7a91da1e48bce68d2009632e61
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-17 18:27:36 -0600

    Add ability to read/write message list from S3

--- daily_message.py
+++ daily_message.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import s3fs
 import re
 import random
 import os
@@ -10,7 +11,7 @@ class MessageList:
         message_list_df):
         self.message_list_df = message_list_df
 
-    # Load message list from CSV file
+    # Load message list from local CSV file
     @classmethod
     def from_csv_file(
         cls,
@@ -20,11 +21,33 @@ class MessageList:
             index_col=0)
         return cls(message_list_df)
 
+    # Load message list from local CSV file
+    @classmethod
+    def from_csv_s3_object(
+        cls,
+        bucket_name,
+        object_name):
+        s3_location = 's3://' + bucket_name +'/' + object_name
+        print('S3 location: {}'.format(s3_location))
+        message_list_df = pd.read_csv(
+            s3_location,
+            index_col=0)
+        return cls(message_list_df)
+
     def to_csv_file(
         self,
         path):
         self.message_list_df.to_csv(
             path)
+
+    def to_csv_s3_object(
+        self,
+        bucket_name,
+        object_name):
+        s3 = s3fs.S3FileSystem(anon=False)
+        s3_location = bucket_name +'/' + object_name
+        with s3.open(s3_location,'w') as f:
+            self.message_list_df.to_csv(f)
 
     # Randomly select a message, return the body, and increment the number of times used
     def next_message(self):
@@ -33,7 +56,7 @@ class MessageList:
         print('Minimum number of times used: {}'.format(min_num_times_used))
         selectable_indices = self.message_list_df.index[self.message_list_df['num_times_used'] == min_num_times_used].tolist()
         print('Selectable indices: {}'.format(selectable_indices))
-        selected_index = random.sample(selectable_indices, 1)[0]
+        selected_index = random.choice(selectable_indices)
         print('Selected index: {}'.format(selected_index))
         selected_message = self.message_list_df.loc[selected_index, 'body']
         print('Selected message: {}'.format(selected_message))
@@ -60,25 +83,30 @@ def parse_html_template_file(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('message_list_path', type=str, help="Message list path (e.g., 'data/sample_message_list.csv')")
+    parser.add_argument('message_list_bucket_name', type=str, help="Message list S3 bucket_name (e.g., 'daily-message')")
+    parser.add_argument('message_list_object_name', type=str, help="Message list S3 object name (e.g., 'sample_message_list.csv')")
     parser.add_argument('html_template_path', type=str, help="HTML template path (e.g., 'html/index_template.html')")
     parser.add_argument('target_html_path', type=str, help="Target HTML path (e.g., 'html/index.html')")
     arguments = parser.parse_args()
 
-    message_list_path = arguments.message_list_path
+    message_list_bucket_name = arguments.message_list_bucket_name
+    message_list_object_name = arguments.message_list_object_name
     html_template_path = arguments.html_template_path
     target_html_path = arguments.target_html_path
-    print('message_list_path: {}'.format(message_list_path))
+    print('message_list_bucket_name: {}'.format(message_list_bucket_name))
+    print('message_list_object_name: {}'.format(message_list_object_name))
     print('html_template_path: {}'.format(html_template_path))
     print('target_html_path: {}'.format(target_html_path))
-    message_list  = MessageList.from_csv_file(
-        message_list_path)
+    message_list  = MessageList.from_csv_s3_object(
+        message_list_bucket_name,
+        message_list_object_name)
     print('Successfully read message list:\n{}'.format(message_list.message_list_df))
     selected_message = message_list.next_message()
     print('Selected message: {}'.format(selected_message))
     print('New message list:\n{}'.format(message_list.message_list_df))
-    message_list.to_csv_file(
-        message_list_path)
+    message_list.to_csv_s3_object(
+        message_list_bucket_name,
+        message_list_object_name)
     parse_html_template_file(
         html_template_path,
         target_html_path,
