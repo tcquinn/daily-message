770836dbc7e9793a4b43a5da64fd9c95761e72fa
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-21 08:53:00 -0600

    Separate main() from module

--- daily_message.py
+++ daily_message.py
@@ -4,7 +4,6 @@ import s3fs
 import re
 import random
 import os
-import argparse
 
 class MessageList:
     def __init__(
@@ -102,42 +101,3 @@ def parse_html_template_s3_object(
     target_html_object.put(
         Body=output_string.encode('utf-8'),
         ContentType='string')
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('message_list_bucket_name', type=str, help="Message list S3 bucket_name (e.g., 'daily-message')")
-    parser.add_argument('message_list_object_name', type=str, help="Message list S3 object name (e.g., 'sample_message_list.csv')")
-    parser.add_argument('html_template_bucket_name', type=str, help="HTML template S3 bucket name (e.g., 'daily-message')")
-    parser.add_argument('html_template_object_name', type=str, help="HTML template S3 object name (e.g., 'index_template.html')")
-    parser.add_argument('target_html_bucket_name', type=str, help="Target HTML S3 bucket name (e.g., 'example.com')")
-    parser.add_argument('target_html_object_name', type=str, help="Target HTML S3 target name (e.g., 'index.html')")
-    arguments = parser.parse_args()
-
-    message_list_bucket_name = arguments.message_list_bucket_name
-    message_list_object_name = arguments.message_list_object_name
-    html_template_bucket_name = arguments.html_template_bucket_name
-    html_template_object_name = arguments.html_template_object_name
-    target_html_bucket_name = arguments.target_html_bucket_name
-    target_html_object_name = arguments.target_html_object_name
-    print('message_list_bucket_name: {}'.format(message_list_bucket_name))
-    print('message_list_object_name: {}'.format(message_list_object_name))
-    print('html_template_bucket_name: {}'.format(html_template_bucket_name))
-    print('html_template_object_name: {}'.format(html_template_object_name))
-    print('target_html_bucket_name: {}'.format(target_html_bucket_name))
-    print('target_html_object_name: {}'.format(target_html_object_name))
-    message_list  = MessageList.from_csv_s3_object(
-        message_list_bucket_name,
-        message_list_object_name)
-    print('Successfully read message list:\n{}'.format(message_list.message_list_df))
-    selected_message = message_list.next_message()
-    print('Selected message: {}'.format(selected_message))
-    print('New message list:\n{}'.format(message_list.message_list_df))
-    message_list.to_csv_s3_object(
-        message_list_bucket_name,
-        message_list_object_name)
-    parse_html_template_s3_object(
-        html_template_bucket_name,
-        html_template_object_name,
-        target_html_bucket_name,
-        target_html_object_name,
-        selected_message)
