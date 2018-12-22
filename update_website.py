688433c6362e47757be5b2fee209580956102378
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-22 17:30:12 -0600

    Rename daily_message module to html_template_parser

--- update_website.py
+++ update_website.py
@@ -1,11 +1,11 @@
-import daily_message
 import message_database
+import html_template_parser
 
 if __name__ == '__main__':
     message_database  = message_database.MessageDatabaseCSVLocal()
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
-    html_template = daily_message.HTMLTemplateLocal()
+    html_template = html_template_parser.HTMLTemplateLocal()
     html_template.create_html([{
         'tag': '@message',
         'value': selected_message}])
