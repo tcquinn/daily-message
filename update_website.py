03328026c967db70b7544acd19e1e1f6754fb6f7
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-22 16:46:57 -0600

    Switch to local files (for debugging purposes)

--- update_website.py
+++ update_website.py
@@ -2,7 +2,7 @@ import daily_message
 import message_database
 
 if __name__ == '__main__':
-    message_database  = message_database.MessageDatabaseCSVS3()
+    message_database  = message_database.MessageDatabaseCSVLocal()
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
-    daily_message.parse_html_template_s3_object(selected_message)
+    daily_message.parse_html_template_file(selected_message)
