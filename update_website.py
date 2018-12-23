3b07d54898bc87d306d8b1f18c57a081a802392a
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-23 09:16:23 -0600

    Switch back to S3

--- update_website.py
+++ update_website.py
@@ -2,10 +2,10 @@ import message_database
 import html_template_parser
 
 if __name__ == '__main__':
-    message_database  = message_database.MessageDatabaseCSVLocal()
+    message_database  = message_database.MessageDatabaseCSVS3()
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
-    html_template = html_template_parser.HTMLTemplateLocal()
+    html_template = html_template_parser.HTMLTemplateS3()
     html_template.create_html([
         {'tag': '@message','value': selected_message['body']},
         {'tag': '@contributor', 'value': selected_message['contributor']}])
