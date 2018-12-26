8205447d4b2896e8bfe3dc3a9f389327314fa5b2
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-26 10:43:22 -0800

    Adjust imports to make it easier to switch between S3 and local

--- update_website.py
+++ update_website.py
@@ -1,11 +1,13 @@
-import message_database
-import html_template_parser
+from message_database import MessageDatabaseCSVS3 as MessageDatabase
+from message_database import MessageDatabaseCSVLocal as MessageDatabase
+from html_template_parser import HTMLTemplateS3 as HTMLTemplate
+# from html_template_parser import HTMLTemplateLocal as HTMLTemplate
 
 if __name__ == '__main__':
-    message_database  = message_database.MessageDatabaseCSVS3()
+    message_database  = MessageDatabase()
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
-    html_template = html_template_parser.HTMLTemplateS3()
+    html_template = HTMLTemplate()
     html_template.create_html([
         {'tag': '@message','value': selected_message['body']},
         {'tag': '@contributor', 'value': selected_message['contributor']}])
