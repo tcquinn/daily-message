65bb542f6ca9edeb5561c2d271e97472a092ff54
Theodore Quinn <tcquinn@tcquinn.org>
2018-12-30 07:42:35 -0800

    Add ability to store selected message for retrieval by other applications

--- message_database.py
+++ message_database.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import boto3
 import s3fs
+import json
 import random
 import os
 
 class MessageDatabaseCSV:
     def next_message(self):
         message_list_df = self.get_dataframe()
-        print('Successfully retrieved message database:\n{}'.format(message_list_df))
+        print('Successfully retrieved message database')
         print('Values for number of times used: {}'.format(message_list_df['num_times_used'].unique()))
         min_num_times_used = message_list_df['num_times_used'].min()
         print('Minimum number of times used: {}'.format(min_num_times_used))
@@ -21,24 +22,24 @@ class MessageDatabaseCSV:
         print('Selected message: {}'.format(selected_message))
         message_list_df.loc[selected_index, 'num_times_used'] += 1
         message_list_df.loc[selected_index, 'last_used'] = pd.Timestamp.now()
-        print('New message database:\n{}'.format(message_list_df))
+        print('Created new message database')
         self.put_dataframe(message_list_df)
         return(selected_message)
 
 class MessageDatabaseCSVS3(MessageDatabaseCSV):
     def __init__(
         self,
-        bucket_name=None,
-        object_name=None):
-        if bucket_name is None:
-            bucket_name = os.environ['MESSAGE_DATABASE_S3_BUCKET_NAME']
-        if object_name is None:
-            object_name = os.environ['MESSAGE_DATABASE_S3_OBJECT_NAME']
-        self.bucket_name = bucket_name
-        self.object_name = object_name
+        message_database_bucket_name=None,
+        message_database_object_name=None):
+        if message_database_bucket_name is None:
+            message_database_bucket_name = os.environ['MESSAGE_DATABASE_S3_BUCKET_NAME']
+        if message_database_object_name is None:
+            message_database_object_name = os.environ['MESSAGE_DATABASE_S3_OBJECT_NAME']
+        self.message_database_bucket_name = message_database_bucket_name
+        self.message_database_object_name = message_database_object_name
 
     def get_dataframe(self):
-        s3_location = 's3://' + self.bucket_name +'/' + self.object_name
+        s3_location = 's3://' + self.message_database_bucket_name +'/' + self.message_database_object_name
         print('S3 location: {}'.format(s3_location))
         message_list_df = pd.read_csv(
             s3_location,
@@ -50,21 +51,22 @@ class MessageDatabaseCSVS3(MessageDatabaseCSV):
         self,
         message_list_df):
         s3 = s3fs.S3FileSystem(anon=False)
-        s3_location = self.bucket_name + '/' + self.object_name
-        with s3.open(s3_location,'w') as f:
-            message_list_df.to_csv(f)
+        s3_location = self.message_database_bucket_name + '/' + self.message_database_object_name
+        bytes_to_write = message_list_df.to_csv(None).encode()
+        with s3.open(s3_location, 'wb') as f:
+            f.write(bytes_to_write)
 
 class MessageDatabaseCSVLocal(MessageDatabaseCSV):
     def __init__(
         self,
-        path=None):
-        if path is None:
-            path = os.environ['MESSAGE_DATABASE_LOCAL_PATH']
-        self.path = path
+        message_database_local_path=None):
+        if message_database_local_path is None:
+            message_database_local_path = os.environ['MESSAGE_DATABASE_LOCAL_PATH']
+        self.message_database_local_path = message_database_local_path
 
     def get_dataframe(self):
         message_list_df = pd.read_csv(
-            self.path,
+            self.message_database_local_path,
             index_col=0,
             parse_dates = ['last_used'])
         return message_list_df
@@ -73,4 +75,64 @@ class MessageDatabaseCSVLocal(MessageDatabaseCSV):
         self,
         message_list_df):
         message_list_df.to_csv(
-            self.path)
+            self.message_database_local_path)
+
+class MessageStoreS3:
+    def __init__(
+        self,
+        message_store_bucket_name=None,
+        message_store_object_name=None):
+        if message_store_bucket_name is None:
+            message_store_bucket_name = os.environ['MESSAGE_STORE_S3_BUCKET_NAME']
+        if message_store_object_name is None:
+            message_store_object_name = os.environ['MESSAGE_STORE_S3_OBJECT_NAME']
+        self.message_store_bucket_name = message_store_bucket_name
+        self.message_store_object_name = message_store_object_name
+
+    def get_message(self):
+        s3 = boto3.resource('s3')
+        message_store_object = s3.Object(
+            self.message_store_bucket_name,
+            self.message_store_object_name)
+        message_string = message_store_object.get()['Body'].read().decode('utf-8')
+        message = json.loads(message_string)
+        return message
+
+    def put_message(
+        self,
+        message):
+        message_string = json.dumps(message)
+        s3 = boto3.resource('s3')
+        message_store_object = s3.Object(
+            self.message_store_bucket_name,
+            self.message_store_object_name)
+        message_store_object.put(
+            Body=message_string.encode('utf-8'),
+            ContentType='string')
+
+class MessageStoreLocal:
+    def __init__(
+        self,
+        message_store_local_path=None):
+        if message_store_local_path is None:
+            message_store_local_path = os.environ['MESSAGE_STORE_LOCAL_PATH']
+        self.message_store_local_path = message_store_local_path
+
+    def get_message(self):
+        message_store = open(
+            self.message_store_local_path,
+            'r')
+        message_string = message_store.read()
+        message_store.close()
+        message = json.loads(message_string)
+        return message
+
+    def put_message(
+        self,
+        message):
+        message_string = json.dumps(message)
+        message_store = open(
+            self.message_store_local_path,
+            'w')
+        message_store.write(message_string)
+        message_store.close()
--- update_website.py
+++ update_website.py
@@ -1,5 +1,7 @@
 from message_database import MessageDatabaseCSVS3 as MessageDatabase
 # from message_database import MessageDatabaseCSVLocal as MessageDatabase
+from message_database import MessageStoreS3 as MessageStore
+# from message_database import MessageStoreLocal as MessageStore
 from html_template_parser import HTMLTemplateS3 as HTMLTemplate
 # from html_template_parser import HTMLTemplateLocal as HTMLTemplate
 
@@ -7,6 +9,8 @@ if __name__ == '__main__':
     message_database  = MessageDatabase()
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
+    message_store = MessageStore()
+    message_store.put_message(selected_message)
     html_template = HTMLTemplate()
     html_template.create_html([
         {'tag': '@message','value': selected_message['body']},
