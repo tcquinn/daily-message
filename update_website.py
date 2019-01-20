2f0d5a4b179670d0ac17966e84bbe620fe939132
Theodore Quinn <tcquinn@tcquinn.org>
2019-01-20 15:11:08 -0600

    Move update website code out of main (because we use it in multiple places

--- update_website.py
+++ update_website.py
@@ -1,17 +1,11 @@
-from message_database import MessageDatabaseCSVS3 as MessageDatabase
-# from message_database import MessageDatabaseCSVLocal as MessageDatabase
-from message_database import MessageStoreS3 as MessageStore
-# from message_database import MessageStoreLocal as MessageStore
-from html_template_parser import HTMLTemplateS3 as HTMLTemplate
-# from html_template_parser import HTMLTemplateLocal as HTMLTemplate
 
-if __name__ == '__main__':
-    message_database  = MessageDatabase()
+def update_website(
+    message_database,
+    message_store,
+    html_template):
     selected_message = message_database.next_message()
     print('Selected message: {}'.format(selected_message))
-    message_store = MessageStore()
     message_store.put_message(selected_message)
-    html_template = HTMLTemplate()
     html_template.create_html([
         {'tag': '@message','value': selected_message['body']},
         {'tag': '@contributor', 'value': selected_message['contributor']}])
---
+++ update_website_test.py
@@ -0,0 +1,16 @@
+import update_website
+from message_database import MessageDatabaseCSVS3 as MessageDatabase
+# from message_database import MessageDatabaseCSVLocal as MessageDatabase
+from message_database import MessageStoreS3 as MessageStore
+# from message_database import MessageStoreLocal as MessageStore
+from html_template_parser import HTMLTemplateS3 as HTMLTemplate
+# from html_template_parser import HTMLTemplateLocal as HTMLTemplate
+
+if __name__ == '__main__':
+    message_database  = MessageDatabase()
+    message_store = MessageStore()
+    html_template = HTMLTemplate()
+    update_website.update_website(
+        message_database,
+        message_store,
+        html_template)
